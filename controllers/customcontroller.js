ec4801016b5be03fbce481bbf4dc3f528d6d7ddf
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-17 21:04:36 +0530

    completed

--- controllers/customcontroller.js
+++ controllers/customcontroller.js
@@ -123,3 +123,62 @@ export const delete15 = async (req, res) => {
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
+
+export const delete1 = async (req, res) => {
+    // CHECKING JWT
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Unauthorized. No token provided" });
+    }
+
+    // CHECKING SHORT URL CODE
+    const urlCode = req.params.urlCode;
+    if (!urlCode) {
+        res.status(404).send("Passed short url is wrong or not found");
+    }
+
+    try {
+        // VERIFY TOKEN
+        const verify = verifyToken(token)
+        if (!verify) {
+            return res.status(401).json({ success: false, message: "Unauthorized token" });
+        }
+
+        // RETRIEVE USER DATA
+        const user = await getUserByDecodedToken(verify.user)
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        let userUrls = await Url.findOne({ user: user._id });
+
+        if (!userUrls) {
+            console.log("User not found.");
+            return;
+        }
+
+        // FIND AND REMOVE THE URL OBJECT
+        let urlFound = false;
+        userUrls.customUrl = userUrls.customUrl.map(subArray => {
+            const updatedSubArray = subArray.filter(urlObj => urlObj.code !== urlCode);
+            if (updatedSubArray.length !== subArray.length) {
+                urlFound = true;
+            }
+            return updatedSubArray;
+        });
+
+        if (!urlFound) {
+            return res.status(404).json({ success: false, message: "URL not found" });
+        }
+
+        // SAVE THE UPDATED DOCUMENT
+        await userUrls.save();
+
+        res.status(200).json({ success: true, message: "data delete success" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
--- routes/custom.js
+++ routes/custom.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { custom15url, get15url ,delete15} from "../controllers/customcontroller.js";
+import { custom15url, get15url ,delete15,delete1} from "../controllers/customcontroller.js";
 
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', custom15url)
 router.get('/mycustom15', get15url)
 router.put('/:urlCode', delete15)
+router.put('/remove1/:urlCode', delete1)
 
 export default router;
