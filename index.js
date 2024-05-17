ecaac473acce9ab39383e0542846c160bb6b6634
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-18 00:21:51 +0530

    added vercel configs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
-import dbConnect from "./config/db.js";
-import baseURL from './routes/index.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import dotenv from 'dotenv';
+import dbConnect from './config/db.js';
+import baseURL from './routes/index.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-//middlewares
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -18,37 +18,29 @@ app.use(cors({
     credentials: true
 }));
 
-const PORT = process.env.PORT || 5000
-
-
+const PORT = process.env.PORT || 5000;
 
 // Server connection
 const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
 
 // DB connection
-dbConnect()
+dbConnect();
 
-
-// Loging requests
+// Logging requests
 app.use((req, res, next) => {
-    console.log(`Route : ${req.method}:${req.originalUrl}`)
-    next()
-})
+    console.log(`Route: ${req.method}:${req.originalUrl}`);
+    next();
+});
 
 // Routes
-app.use("/api", baseURL)
-
-
+app.use('/api', baseURL);
 
 // Handle Errors
 process.on('unhandledRejection', (reason, p) => {
-    console.error('Unhandled Rejection at:', p, 'reason:', reason)
-    server.close()
-    process.exit(1)
+    console.error('Unhandled Rejection at:', p, 'reason:', reason);
+    server.close(() => process.exit(1));
 });
 process.on('uncaughtException', (e) => {
-    console.error('Uncaught exception at:', e)
-
-    server.close()
-    process.exit(1)
+    console.error('Uncaught exception:', e);
+    server.close(() => process.exit(1));
 });
