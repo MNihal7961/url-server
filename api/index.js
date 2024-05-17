10d48a49844d464f0f1288c15829955cffce452e
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-18 00:38:31 +0530

    done

---
+++ api/index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import dbConnect from '../config/db.js'
+import baseUrl from '../routes/index.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+const PORT = process.env.PORT || 5000;
+
+// Server connection
+const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+// DB connection
+dbConnect();
+
+// Logging requests
+app.use((req, res, next) => {
+    console.log(`Route: ${req.method}:${req.originalUrl}`);
+    next();
+});
+
+// Routes
+app.use('/api', baseUrl);
+
+// Handle Errors
+process.on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at:', p, 'reason:', reason);
+    server.close(() => process.exit(1));
+});
+process.on('uncaughtException', (e) => {
+    console.error('Uncaught exception:', e);
+    server.close(() => process.exit(1));
+});
--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import dbConnect from './config/db.js';
-import baseURL from './routes/index.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import dbConnect from './config/db.js';
+// import baseURL from './routes/index.js';
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+// // Middlewares
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//     origin: true,
+//     credentials: true
+// }));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-// Server connection
-const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+// // Server connection
+// const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
 
-// DB connection
-dbConnect();
+// // DB connection
+// dbConnect();
 
-// Logging requests
-app.use((req, res, next) => {
-    console.log(`Route: ${req.method}:${req.originalUrl}`);
-    next();
-});
+// // Logging requests
+// app.use((req, res, next) => {
+//     console.log(`Route: ${req.method}:${req.originalUrl}`);
+//     next();
+// });
 
-// Routes
-app.use('/api', baseURL);
+// // Routes
+// app.use('/api', baseURL);
 
-// Handle Errors
-process.on('unhandledRejection', (reason, p) => {
-    console.error('Unhandled Rejection at:', p, 'reason:', reason);
-    server.close(() => process.exit(1));
-});
-process.on('uncaughtException', (e) => {
-    console.error('Uncaught exception:', e);
-    server.close(() => process.exit(1));
-});
+// // Handle Errors
+// process.on('unhandledRejection', (reason, p) => {
+//     console.error('Unhandled Rejection at:', p, 'reason:', reason);
+//     server.close(() => process.exit(1));
+// });
+// process.on('uncaughtException', (e) => {
+//     console.error('Uncaught exception:', e);
+//     server.close(() => process.exit(1));
+// });
